2d22e6e4573724297052c819294a8bf1df5ac197
Zachery Miranda <zmiranda@nat-tvwna-inside-visitornet101-b-15161.princeton.org>
2017-11-11 10:28:15 -0500

    tic tac toe yay

---
+++ stdlib/exsmiley/slack-app/functions/commands/ttt.js
@@ -0,0 +1,65 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /ttt
+*
+*   Tells the user tic tac toe information.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  lib.exsmiley.teammaker['@dev']({name: user}, function (err, team) {
+
+    let loc = parseInt(text);
+    let reset = text.includes('reset');
+
+    if (err || (isNaN(loc) || loc < 1 || loc > 9) && !reset) {
+      // handle it
+      callback(err, {
+          response_type: 'in_channel',
+          text: 'location out of bounds (0->8) or invalid command (location number or "reset")!'
+        });
+    }
+
+    // makes sure no errors occur
+    if(reset) {
+      loc = 0;
+    }
+
+    lib.exsmiley.ttt['@dev']({team: team, loc: loc, reset: reset}, function(err, result) {
+
+      if (!result['success']) {
+        // handle it
+        callback(null, {
+          response_type: 'in_channel',
+          text: '(Tic Tac Toe) ' + result['text']
+        });
+      }
+      console.log(result)
+      let text = result['text'];
+
+      if(text.includes('|')) {
+        let color = 'Red';
+        if(team != 0) {
+          color = 'Blue';
+        }
+        text = `(Tic Tac Toe) <@${user}>:${color}` + text;
+      } else {
+        text = '(Tic Tac Toe) ' + text;
+      }
+
+      callback(null, {
+        response_type: 'in_channel',
+        text: text
+      });
+    });
+    
+
+  });
+
+};
--- stdlib/exsmiley/ttt/functions/__main__.js
+++ stdlib/exsmiley/ttt/functions/__main__.js
@@ -1,27 +1,93 @@
-let marked = [null, null, null, null, null, null, null, null, null];
-let win = false;
+let marked = ['~', '~', '~', '~', '~', '~', '~', '~', '~'];
+let gameOver = false;
+let lastTeam = Math.round(Math.random());
+
+function markedToBoard(marked) {
+	let marked2 = [];
+	for(m of marked) {
+		if(m == 0) {
+			marked2.push('R');
+		} else if(m == 1) {
+			marked2.push('B');
+		} else {
+			marked2.push('~');
+		}
+	}
+	return `|${marked2[0]}${marked2[1]}${marked2[2]}|\n|${marked2[3]}${marked2[4]}${marked2[5]}|\n|${marked2[6]}${marked2[7]}${marked2[8]}|\n`
+}
+
+// returns false if there is still an empty location or a team won
+function isTie(marked, gameOver) {
+	if(gameOver) {
+		return false;
+	} else {
+		let tie = true;
+		for(let i = 0; i < marked.length; i++) {
+			if(marked[i] == '~') {
+				tie = false;
+			}
+		}
+		return tie;
+	}
+}
+
 /**
-* A basic Hello World function
+* The tic tac toe game
 * @param {integer} team What token is placed for that team
 * @param {integer} loc Where the token is placed
-* @returns {String}
+* @param {boolean} reset if the game should be reset
+* @returns {object} {text: str, success: bool}
 */
-module.exports = (team, loc, context, callback) => {
-	if (marked[loc]){
-		callback(null, `invalid`);
+module.exports = (team, loc, reset=false, context, callback) => {
+	if(reset && gameOver) {
+		lastTeam = Math.round(Math.random());
+		let color = 'Red';
+	    if(lastTeam != 0) {
+	      color = 'Blue';
+	    }
+		callback(null, {text: `Successfully Reset! It is ${color}'s turn!`, success: true});
+	} else if(gameOver) {
+		callback(null, {text: 'Game already over\n' + markedToBoard(marked), success: false})
+	} else if(reset) {
+		callback(null, {text: "Can't reset: game not over\n" + markedToBoard(marked), success: false})
+	} else if(lastTeam == team) {
+		let color = 'Red';
+	    if(team != 0) {
+	      color = 'Blue';
+	    }
+		callback(null, {text: `It is not ${color}'s turn.`, success: false})
+	} else if (marked[loc-1] != '~'){
+		callback(null, {text: `Location ${loc} already marked!\n` + markedToBoard(marked), success: false});
+	} else {
+		marked[loc-1] = team;
+		lastTeam = team;
+
+		marked[0] == marked[1] && marked[1] == marked[2] && marked[1] != '~' ? gameOver = true: "N/A";
+		marked[3] == marked[4] && marked[4] == marked[5] && marked[4] != '~' ? gameOver = true: "N/A";
+		marked[6] == marked[7] && marked[7] == marked[8] && marked[7] != '~' ? gameOver = true: "N/A";
+
+		marked[0] == marked[3] && marked[3] == marked[6] && marked[3] != '~' ? gameOver = true: "N/A";
+		marked[1] == marked[4] && marked[4] == marked[7] && marked[4] != '~' ? gameOver = true: "N/A";
+		marked[2] == marked[5] && marked[5] == marked[8] && marked[5] != '~' ? gameOver = true: "N/A";
+
+		marked[0] == marked[4] && marked[4] == marked[8] && marked[4] != '~' ? gameOver = true: "N/A";
+		marked[2] == marked[4] && marked[4] == marked[6] && marked[4] != '~' ? gameOver = true: "N/A";
+
+		let color = 'Red';
+	    if(team != 0) {
+	      color = 'Blue';
+	    }
+
+	    let text = ` played at location: ${loc}\n` + markedToBoard(marked);
+	    if(gameOver) {
+	    	text += `${color} won the game!`
+	    }
+
+	    if(isTie(marked, gameOver)) {
+	    	gameOver = true;
+	    	text += 'Tie game!'
+	    }
+
+		callback(null, {text: text, success: true});
 	}
-	marked[loc] = team;
-
-	marked[0] == marked[1] && marked[1] == marked[2] ? win = true: "N/A";
-	marked[3] == marked[4] && marked[4] == marked[5] ? win = true: "N/A";
-	marked[6] == marked[7] && marked[7] == marked[8] ? win = true: "N/A";
-
-	marked[0] == marked[3] && marked[3] == marked[6] ? win = true: "N/A";
-	marked[1] == marked[4] && marked[4] == marked[7] ? win = true: "N/A";
-	marked[2] == marked[5] && marked[5] == marked[8] ? win = true: "N/A";
-
-	marked[0] == marked[4] && marked[4] == marked[8] ? win = true: "N/A";
-	marked[2] == marked[4] && marked[4] == marked[6] ? win = true: "N/A";
-
-	callback(null, 'valid');
 };
