956ecf73baebd30923d5c17eb5a3b7df692a1597
Zachery Miranda <zmiranda@nat-tvwna-inside-visitornet101-b-15161.princeton.org>
2017-11-11 02:58:10 -0500

    we got a teammaker service bros

---
+++ stdlib/exsmiley/teammaker/functions/__main__.js
@@ -0,0 +1,19 @@
+/**
+* Creates a hashcode from a string. Thank you based lordvlad <3
+* (https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery)
+* @returns {integer} hashcode
+*/
+function hashCode(s) {
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
+/**
+* Maps usernames to a binary team number (0/1)
+* @param {string} name user who we are finding the name of
+* @returns {integer} number of team
+*/
+module.exports = (name = 'Bob', context, callback) => {
+
+    callback(null, Math.abs(hashCode(name)) % 2);
+
+};
